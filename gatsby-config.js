a6a7ce51406e26f3cc0f28f5a84d1ec651d76660
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-07 01:23:56 +0900

    add types

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-graphql-codegen",
+      options: {
+        fileName: `types/graphql-types.d.ts`,
+      },
+    },
   ],
 }
