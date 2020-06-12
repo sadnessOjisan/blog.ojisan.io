94ae62d2e73cb231f90ae8838850c38d18c7d387
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-12 14:36:38 +0900

    add babel article

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import styles from "./header.module.css"
+import twitter from "../images/twitter.svg"
+import github from "../images/github.svg"
 
 interface IProps {
   siteTitle: string
@@ -9,8 +11,16 @@ interface IProps {
 const Header: React.FC<IProps> = ({ siteTitle }) => (
   <header className={styles.header}>
     <h1 className={styles.title}>
-      <Link to="/">{siteTitle || ""}</Link>
+      <Link to="/">😭</Link>
     </h1>
+    <div>
+      <a href="https://twitter.com/sadnessOjisan">
+        <img src={twitter} className={styles.icon}></img>
+      </a>
+      <a href="https://github.com/sadnessOjisan/blog.ojisan.io">
+        <img src={github} className={styles.icon}></img>
+      </a>
+    </div>
   </header>
 )
 
