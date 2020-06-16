da782ac8416d984acf7289a6bef17d312d212877
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-17 00:27:41 +0900

    use stand

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
