7a19e404844370fc905101829fe28f571db0f058
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-19 02:13:21 +0900

    disolve

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,8 @@
+import React from "react"
+import Transition from "./src/components/Transition"
 require("prismjs/themes/prism-tomorrow.css")
 require("./src/vendor/css/code.css")
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Transition {...props}>{element}</Transition>
+}
