687e15d905a4425f6bf9887e98046bcb25789764
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-19 16:09:02 +0900

    card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 import "../vendor/css/reset.css"
 import "../vendor/css/base.css"
@@ -11,41 +11,56 @@ interface IProps {
   data: AllBlogsQuery
 }
 
-const IndexPage: React.FC<IProps> = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 style={{ fontSize: "24px", marginTop: "12px", textAlign: "center" }}>
-      そろそろ飽きそうブログ
-    </h1>
-    {data.allMarkdownRemark.nodes.map(node => (
-      <li
+const IndexPage: React.FC<IProps> = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 style={{ fontSize: "24px", marginTop: "12px", textAlign: "center" }}>
+        BLOG
+      </h1>
+      <div>
+        NEWS<br></br>
+        {data.newses.nodes.map(news => {
+          return (
+            <ul>
+              <li>{news.frontmatter?.created}</li>
+              <li>{news.frontmatter?.title}</li>
+            </ul>
+          )
+        })}
+      </div>{" "}
+      BLOGS<br></br>
+      {data.blogs.nodes.map(node => (
+        <li
+          style={{
+            marginTop: "12px",
+            color: "blue",
+            textDecoration: "underline",
+          }}
+        >
+          <Link to={node.frontmatter?.path || "/"}>
+            {node.frontmatter?.title}
+          </Link>
+        </li>
+      ))}
+      <p
         style={{
-          marginTop: "12px",
-          color: "blue",
-          textDecoration: "underline",
+          color: "red",
+          fontSize: "40px",
+          marginTop: "40px",
+          fontWeight: "bold",
         }}
       >
-        <Link to={node.frontmatter?.path || "/"}>
-          {node.frontmatter?.title}
-        </Link>
-      </li>
-    ))}
-    <p
-      style={{
-        color: "red",
-        fontSize: "40px",
-        marginTop: "40px",
-        fontWeight: "bold",
-      }}
-    >
-      工事中
-    </p>
-  </Layout>
-)
+        工事中
+      </p>
+    </Layout>
+  )
+}
 
 export const pageQuery = graphql`
   query AllBlogs {
-    allMarkdownRemark
+    blogs: allMarkdownRemark
     (
       filter: {fileAbsolutePath: {regex: "/(\/src\/contents)/.*\\.md$/"}}) {
       nodes {
@@ -53,6 +68,17 @@ export const pageQuery = graphql`
         frontmatter {
           title
           path
+        }
+      }
+    }
+
+    newses: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(/src/news)/.*\\.md$/"}}) {
+      nodes {
+        frontmatter {
+          title
+          description
+          created
+          tags
         }
       }
     }
