75d65f7a00fe79acef888722f579e752ddc9ab98
sadness_ojisan <sadness.ojisan@gmail.com>
2020-08-03 00:44:35 +0900

    add style

--- src/components/common/drawer-contents.tsx
+++ src/components/common/drawer-contents.tsx
@@ -2,9 +2,22 @@ import * as React from "react"
 import styles from "./drawer-contents.module.css"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import { TagsIndicesQuery } from "../../../types/graphql-types"
-import { Button } from "@material-ui/core"
+import { Button, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles({
+  root: {
+    border: 0,
+    borderRadius: 3,
+    height: 48,
+    width: "100%",
+  },
+  label: {
+    justifyContent: "left",
+  },
+})
 
 const DrawerContents: React.FC = () => {
+  const classes = useStyles()
   const {
     allMarkdownRemark,
   }: {
@@ -30,22 +43,39 @@ const DrawerContents: React.FC = () => {
       <ul className={styles.block}>
         <li className={styles.item}>
           <Link to="/">
-            <Button>
+            <Button className={`${classes.root} ${classes.label}`}>
               <a>Top</a>
             </Button>
           </Link>
         </li>
         <li className={styles.item}>
           <Link to="/tags">
-            <Button>
+            <Button className={`${classes.root} ${classes.label}`}>
               <a>Tag一覧</a>
             </Button>
           </Link>
         </li>
         <li className={styles.item}>
           <Link to="/news">
-            <Button>
+            <Button className={`${classes.root} ${classes.label}`}>
               <a>News一覧</a>
+            </Button>
+          </Link>
+        </li>
+      </ul>{" "}
+      <h2 className={styles.title}>Link</h2>
+      <ul className={styles.block}>
+        <li className={styles.item}>
+          <Link to="/rss.xml">
+            <Button className={`${classes.root} ${classes.label}`}>
+              <a>RSS</a>
+            </Button>
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link to="https://github.com/sadnessOjisan/blog.ojisan.io">
+            <Button className={`${classes.root} ${classes.label}`}>
+              <a>Github</a>
             </Button>
           </Link>
         </li>
@@ -55,7 +85,7 @@ const DrawerContents: React.FC = () => {
         {allMarkdownRemark.group.map(tag => (
           <li className={styles.item}>
             <Link to={`/tags/${tag.fieldValue}`}>
-              <Button>
+              <Button className={`${classes.root} ${classes.label}`}>
                 <a>
                   {tag.fieldValue}({tag.totalCount})
                 </a>
@@ -63,23 +93,6 @@ const DrawerContents: React.FC = () => {
             </Link>
           </li>
         ))}
-      </ul>
-      <h2 className={styles.title}>Link</h2>
-      <ul className={styles.block}>
-        <li className={styles.item}>
-          <Link to="/rss.xml">
-            <Button>
-              <a>RSS</a>
-            </Button>
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="https://github.com/sadnessOjisan/blog.ojisan.io">
-            <Button>
-              <a>Github</a>
-            </Button>
-          </Link>
-        </li>
       </ul>
     </div>
   )
