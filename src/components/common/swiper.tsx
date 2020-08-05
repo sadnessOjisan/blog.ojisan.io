8dbf525a39cbc4cdbf54952f5b852d7d0babad25
sadness_ojisan <sadness.ojisan@gmail.com>
2020-08-06 05:08:09 +0900

    swipe margin

--- src/components/common/swiper.tsx
+++ src/components/common/swiper.tsx
@@ -1,8 +1,13 @@
 import * as React from "react"
+import cn from "classnames"
 import styles from "./swiper.module.css"
 
-const Swiper: React.FC = props => (
-  <div className={styles.wrapper}>{props.children}</div>
+interface IProps {
+  className: string
+}
+
+const Swiper: React.FC<IProps> = props => (
+  <div className={cn(props.className, styles.wrapper)}>{props.children}</div>
 )
 
 export default Swiper
