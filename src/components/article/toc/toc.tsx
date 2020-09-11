5b185b81c4506b80845a1e11f127aa0b90b0a48b
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-12 06:53:57 +0900

    update: mobile toc

--- src/components/article/toc/toc.tsx
+++ src/components/article/toc/toc.tsx
@@ -1,23 +1,57 @@
 /** FIXME: useStaticQuery で toc を実現したいわね */
 
 import * as React from "react"
-import cn from "classnames"
-import styles from "./toc.module.css"
+import styled from "styled-components"
 
 interface IProps {
   tableOfContents?: string | null
   className?: string
 }
 
-const Toc: React.FC<IProps> = props => {
+const Component: React.FC<IProps> = props => {
   return props.tableOfContents ? (
     <nav
-      className={cn(props.className, styles.toc)}
+      className={props.className}
       dangerouslySetInnerHTML={{ __html: props.tableOfContents }}
     />
   ) : (
-    <div></div>
-  )
+      <div></div>
+    )
 }
 
-export default Toc
+const StyledComponent = styled(Component)`
+> ul {
+  position: sticky;
+  /* 9vh は headerの高さ分 */
+  top: calc(9vh + 32px);
+  color: rgba(0, 0, 0, 0.84);
+  font-size: 16px;
+  margin-bottom: 24px;
+  overflow-y: auto;
+  max-height: 75vh;
+  background-color: white;
+  border-radius: 8px;
+  padding: 12px;
+  margin-top: 30vh;
+}
+
+& ul {
+  margin-left: 16px;
+}
+
+a:hover {
+  background-color: aliceblue;
+}
+
+& a {
+  display: block;
+  padding: 6px;
+  padding-left: 12px;
+}
+
+@media screen and (max-width: 1024px) {
+    display: none;
+}
+`
+
+export default StyledComponent
