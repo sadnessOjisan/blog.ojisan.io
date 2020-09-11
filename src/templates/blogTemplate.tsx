4ee5b2badb937765b814c2b6a0b04ef50ab204fe
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-12 07:52:47 +0900

    update: templae

--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Helmet } from "react-helmet"
 import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
-import styles from "./blogTemplate.module.css"
 import { BlogTemplateQuery } from "../../types/graphql-types"
 import Layout from "../components/common/layout"
 import SEO from "../components/common/seo"
@@ -14,14 +13,23 @@ import { Tag } from "../components/indices/tag"
 import { UserType } from "../type"
 import { Card } from "../components/indices/card"
 import Swiper from "../components/common/swiper"
+import styled from "styled-components"
 
-interface IProps {
+interface IPassedProps {
   data: BlogTemplateQuery
   pageContext: UserType
 }
 
-export default function Template({ data, pageContext }: IProps) {
-  const [isOpen, setTocOpenerState] = React.useState(false)
+interface IContainerProps {
+  isOpen: boolean;
+  setTocOpenerState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface IProps extends IPassedProps, IContainerProps {
+  className?: string;
+}
+
+const Component: React.FC<IProps> = ({ data, pageContext, className, isOpen, setTocOpenerState }) => {
   const { post, latestPosts, favoriteArticles } = data
   return (
     <Layout>
@@ -39,6 +47,7 @@ export default function Template({ data, pageContext }: IProps) {
               description={post.excerpt.slice(0, 100)}
               image={post.frontmatter.visual?.childImageSharp?.fluid?.src}
             />
+            {/* はてぶ炎上プロテクト: frontmatterのisProtectをtrueにすると動作する */}
             {post.frontmatter.isProtect && (
               <Helmet
                 meta={[
@@ -49,51 +58,53 @@ export default function Template({ data, pageContext }: IProps) {
                 ]}
               />
             )}
-            <div className={isOpen && styles.modalOpenBody}>
-              <div className={styles.main}>
+            <div className={className}>
+              <div className={'main'}>
                 <Social
-                  className={styles.socials}
+                  className={'socials'}
                   path={post.frontmatter.path}
                   title={post.frontmatter.title}
                   dateYYYYMMDD={post.frontmatter.created.replace(/-/g, "")}
                 ></Social>
-                <div className={styles.contentWrapper}>
-                  <div className={styles.articleHeader}>
-                    <h1 className={styles.headline}>{post.frontmatter.title}</h1>
-                    <h2 className={styles.date}>
+                <div className={'contentWrapper'}>
+                  <div className={'articleHeader'}>
+                    <h1 className={'headline'}>{post.frontmatter.title}</h1>
+                    <h2 className={'date'}>
                       {post.frontmatter.created}(created)
                     {post.frontmatter.updated &&
                         `/${post.frontmatter.updated}(updated)`}
                     </h2>
-                    <div className={styles.tags}>
+                    {/* TODO: tasgsコンポーネントで置き換える */}
+                    {/* FIXME: Cardのtagとclassが衝突する事故 */}
+                    <div className={'article-tags'}>
                       {post.frontmatter.tags.map(
                         tag =>
                           tag && (
                             <Link to={`/tags/${tag}`}>
                               <a>
-                                <Tag className={styles.tag} name={tag}></Tag>
+                                <Tag className={'article-tag'} name={tag}></Tag>
                               </a>
                             </Link>
                           )
                       )}
                     </div>
-                    <div className={styles.userRow}>
+                    <div className={'userRow'}>
                       <Link to={`/users/${pageContext.id}`}>
                         <UserImage
-                          className={styles.userImage}
+                          className={'userImage'}
                           filename={pageContext.image}
                           alt={pageContext.image}
                         ></UserImage>{" "}
                       </Link>
-                      <div className={styles.userInfoRow}>
+                      <div className={'userInfoRow'}>
                         <Link to={`/users/${pageContext.id}`}>
-                          <div className={styles.userLink}>
+                          <div className={'userLink'}>
                             {pageContext.name}
                           </div>
                         </Link>
                         <a
                           href={`https://twitter.com/${pageContext.name}`}
-                          className={styles.followButton}
+                          className={'followButton'}
                           target="_blank"
                           rel="noopener"
                         >
@@ -113,14 +124,14 @@ export default function Template({ data, pageContext }: IProps) {
                     />
                   )}
                   <div
-                    className={styles.content}
+                    className={'content'}
                     dangerouslySetInnerHTML={{ __html: post.html }}
                   />
                 </div>
-
+                {/* TODO: hiddenかどうかはこの階層のCSSで制御すべき */}
                 <Toc
                   tableOfContents={post.tableOfContents}
-                  className={styles.tocwrapper}
+                  className={'tocwrapper'}
                 ></Toc>
                 <TocMobile
                   isOpen={isOpen}
@@ -133,28 +144,28 @@ export default function Template({ data, pageContext }: IProps) {
                   dateYYYYMMDD={post.frontmatter.created.replace(/-/g, "")}
                 ></TocMobile>
               </div>
-              <div className={styles.posts}>
+              <div className={'posts'}>
                 <div
                   style={{
                     maxWidth: "95vw",
                     marginLeft: "auto",
                   }}
                 >
-                  <h3 className={styles.sectionTitle}>最新の記事</h3>
-                  <Swiper className={styles.swiper}>
+                  <h3 className={'sectionTitle'}>最新の記事</h3>
+                  <Swiper className={'swiper'}>
                     {latestPosts.nodes.map(node => (
                       <Card
                         data={node.frontmatter}
-                        className={styles.card}
+                        className={'card'}
                       ></Card>
                     ))}
                   </Swiper>
-                  <h3 className={styles.sectionTitle}>人気の記事</h3>
-                  <Swiper className={styles.swiper}>
+                  <h3 className={'sectionTitle'}>人気の記事</h3>
+                  <Swiper className={'swiper'}>
                     {favoriteArticles.nodes.map(node => (
                       <Card
                         data={node.frontmatter}
-                        className={styles.card}
+                        className={'card'}
                       ></Card>
                     ))}
                   </Swiper>
@@ -168,6 +179,296 @@ export default function Template({ data, pageContext }: IProps) {
     </Layout>
   )
 }
+
+const StyledComponent = styled(Component)`
+& .main {
+  display: flex;
+  justify-content: center;
+}
+
+& .main > .socials {
+  width: 5%;
+  padding-top: 32px;
+  padding-right: 36px;
+}
+
+& .main > .tocwrapper {
+  width: 25%;
+  padding-left: 16px;
+  padding-top: 32px;
+}
+
+& .articleHeader {
+  max-width: 768px;
+  margin-bottom: 12px;
+  padding: 8px;
+  min-height: 20vh;
+}
+
+& .headline {
+  color: #2c2e31;
+  line-height: 48px;
+  font-size: 32px;
+  margin: 8px 0px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 1024px) {
+  & .headline {
+    margin: 8px 0px;
+    font-size: 28px;
+    line-height: 36px;
+  }
+}
+
+& .date {
+  color: rgba(0, 0, 0, 0.54);
+  font-size: 16px;
+  margin-bottom: 12px;
+  padding: 0 8px;
+}
+
+& .article-tags {
+  display: flex;
+  margin: 8px 0px;
+}
+
+& .article-tag {
+  margin-right: 12px;
+  margin-bottom: 8px;
+}
+
+& .userRow {
+  display: flex;
+  align-items: center;
+  margin-top: 16px;
+}
+
+& .userImage {
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  border: solid 1px #ccc;
+}
+
+& .userInfoRow {
+  display: flex;
+  flex-direction: column;
+  margin-left: 12px;
+}
+
+& .userLink {
+  text-decoration: underline;
+  font-size: 16px;
+}
+
+& .followButton {
+  border: solid 1px #2196f3;
+  color: #2196f3;
+  /* TIPS: 引き延ばし防止, https://www.webcreatorbox.com/tech/flexbox-tips */
+  align-self: flex-start;
+  margin-top: 4px;
+  padding: 4px;
+  border-radius: 4px;
+  font-size: 12px;
+}
+
+& .followButton:hover {
+  display: block;
+  background-color: white;
+}
+
+@media screen and (max-width: 1024px) {
+  .contentWrapper {
+    width: 100%;
+  }
+
+  .socials {
+    display: none;
+  }
+  .content {
+    width: 90%;
+    margin: 0 auto;
+  }
+}
+
+// .contentが記事本体
+& .content {
+  max-width: 768px;
+  padding: 32px;
+  color: #2c2e31;
+  background-color: white;
+}
+
+@media screen and (max-width: 1024px) {
+  .content {
+    padding: 0px;
+    margin-top: 16px;
+    border-radius: 8px;
+    background-color: initial;
+    box-shadow: initial;
+  }
+}
+
+& .content > p,
+& .content > ul {
+  font-size: 16px;
+  line-height: 1.6;
+}
+
+& .content > h1 {
+  font-size: 32px;
+  line-height: 32px;
+  margin-top: 40px;
+  font-weight: bold;
+}
+
+& .content > h2 {
+  font-size: 28px;
+  margin-top: 50px;
+  font-weight: bold;
+}
+
+& .content > h3 {
+  font-size: 22px;
+  margin-top: 20px;
+  font-weight: bold;
+}
+
+& .content > h4 {
+  font-size: 20px;
+  margin-top: 16px;
+  font-weight: bold;
+}
+
+@media screen and (max-width: 1024px) {
+  & .content > h1 {
+    font-size: 30px;
+    line-height: 32px;
+    margin-top: 3px;
+  }
+
+  & .content > h2 {
+    font-size: 28px;
+    margin-top: 30px;
+  }
+
+  & .content > h3 {
+    font-size: 24px;
+    margin-top: 16px;
+  }
+
+  & .content > h4 {
+    font-size: 20px;
+    margin-top: 12px;
+  }
+}
+
+& .content > blockquote {
+  font-style: italic;
+}
+
+& .content > p,
+& .content > img,
+& .content > div,
+& .content > ul,
+& .content > blockquote {
+  margin-bottom: 20px;
+  word-break: break-all;
+}
+
+& .content > h1,
+& .content > h2,
+& .content > h3,
+& .content > h4 {
+  margin-bottom: 12px;
+  word-break: break-all;
+}
+
+& .content li {
+  margin-bottom: 8px;
+}
+
+& .content ul {
+  list-style: circle inside;
+  margin-left: 16px;
+}
+
+& .content > ul {
+  list-style: square inside;
+}
+
+& .content li p {
+  display: inline;
+}
+
+& .content a {
+  color: #368ccb;
+}
+
+& .content iframe {
+  display: block;
+  margin: 32px auto;
+}
+
+& .content iframe {
+  width: 560px;
+  height: 315px;
+}
+
+@media screen and (max-width: 1024px) {
+  & .content iframe {
+    width: initial;
+    height: initial;
+  }
+}
+
+// 記事以下
+& .posts {
+  margin-top: 60px;
+  padding: 24px;
+  padding-right: 0;
+  background-color: white;
+}
+
+& .sectionTitle {
+  font-size: 32px;
+  margin: 20px 0px;
+  font-weight: bold;
+}
+
+& .card {
+  flex-shrink: 0;
+  width: 30%;
+  border: solid 3px #ccc;
+  display: block;
+}
+
+& .card:not(:first-child) {
+  margin-left: 12px;
+}
+
+& .swiper {
+  scroll-margin-right: 12px;
+  margin-right: 12px;
+}
+
+@media screen and (max-width: 1024px) {
+  & .card {
+    width: 90%;
+    border: solid 3px #ccc;
+  }
+}
+`
+
+const ContainerComponent: React.FC<IPassedProps> = (props) => {
+  const [isOpen, setTocOpenerState] = React.useState(false)
+  const containerProps = { isOpen, setTocOpenerState }
+  return <StyledComponent {...props} {...containerProps}></StyledComponent>
+}
+
+export default ContainerComponent;
+
 export const pageQuery = graphql`
   query BlogTemplate($path: String!) {
     post: markdownRemark(frontmatter: { path: { eq: $path } }) {
