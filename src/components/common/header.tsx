ec326fabec9dfa9d6da85e646ccd1e734310490b
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-03 06:41:09 +0900

    fix fill menu

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -56,7 +56,7 @@ const Component: React.FC<IProps> = ({
       >
         <span>
           {/* menu */}
-          <svg viewBox="0 0 24 24" width={24} height={24}>
+          <svg viewBox="0 0 24 24" width={24} height={24} fill="white">
             <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
           </svg>
         </span>
