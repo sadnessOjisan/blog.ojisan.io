7be9362c311c71ac1c5b38aa33f091371bbb65fd
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-02 21:30:36 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,53 +9,62 @@ import { Card } from "../components/indices/card"
 interface IProps {
   data: AllBlogsQuery
 }
-const IndexPage: React.FC<IProps> = ({ data }) => {
+const Component: React.FC<IProps> = ({ data, className }) => {
   return (
-    <Layout>
-      <SEO title={data.site?.siteMetadata?.title || "HOME"} />
-      <Cards>
-        {data.blogs.nodes.map(node =>
-          node.frontmatter?.path ? (
-            <StyledCard
-              key={node.frontmatter.path}
-              excerpt={node.excerpt}
-              data={node.frontmatter}
-            ></StyledCard>
-          ) : (
-            <div>invalid data</div>
-          )
-        )}
-      </Cards>
-    </Layout>
+    <div className={className}>
+      <Layout>
+        <SEO title={data.site?.siteMetadata?.title || "HOME"} />
+        <div className="cards">
+          {data.blogs.nodes.map(node =>
+            node.frontmatter?.path ? (
+              <Card
+                className="card"
+                key={node.frontmatter.path}
+                excerpt={node.excerpt}
+                data={node.frontmatter}
+              ></Card>
+            ) : (
+              <div>invalid data</div>
+            )
+          )}
+        </div>
+      </Layout>
+    </div>
   )
 }
 
-const Cards = styled.div`
-  margin: 24px auto;
-  padding: 5px;
-  width: 90%;
-  column-count: 3;
-  column-gap: 0;
-  max-width: 1024px;
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
+const StyledComponent = styled(Component)`
+  & .cards {
+    margin: 24px auto;
+    padding: 5px;
+    width: 90%;
+    column-count: 3;
+    column-gap: 0;
+    max-width: 1024px;
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+
+    & .card {
+      margin: 16px;
+      margin-top: 0;
+      -webkit-column-break-inside: avoid;
+      page-break-inside: avoid;
+      break-inside: avoid;
+      box-shadow: 8px 12px 10px -6px rgba(0, 0, 0, 0.3);
+      display: inline-block;
+      @media (max-width: 1024px) {
+        margin-bottom: 16px;
+      }
+    }
   }
 `
 
-const StyledCard = styled(Card)`
-  margin: 16px;
-  margin-top: 0;
-  -webkit-column-break-inside: avoid;
-  page-break-inside: avoid;
-  break-inside: avoid;
-  box-shadow: 8px 12px 10px -6px rgba(0, 0, 0, 0.3);
-  display: inline-block;
-  @media (max-width: 1024px) {
-    margin-bottom: 16px;
-  }
-`
+const ContainerComponent: React.FC<IProps> = ({ children, data }) => {
+  return <StyledComponent data={data}>{children}</StyledComponent>
+}
 
 export const pageQuery = graphql`
   query AllBlogs {
@@ -88,4 +97,4 @@ export const pageQuery = graphql`
   }
 `
 
-export default IndexPage
+export default ContainerComponent
