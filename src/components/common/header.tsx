f122b75ea5fe4d0089c5f36d3700ea70f338cbf3
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-03 06:39:10 +0900

    fix

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -27,10 +27,10 @@ const Component: React.FC<IProps> = ({
       <a href="/rss.xml" aria-label="rssへのリンク">
         <button aria-label="rssアイコン" className="button">
           <span>
-            <React.Fragment>
+            <svg viewBox="0 0 24 24" width={24} height={24} fill="white">
               <circle cx="6.18" cy="17.82" r="2.18" />
               <path d="M4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z" />
-            </React.Fragment>
+            </svg>
           </span>
         </button>
       </a>
@@ -41,7 +41,7 @@ const Component: React.FC<IProps> = ({
         <button aria-label="githubアイコン" className="button">
           <span>
             {/* GitHub */}
-            <svg viewBox="0 0 24 24" width={24} height={24}>
+            <svg viewBox="0 0 24 24" width={24} height={24} fill="white">
               <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" />
             </svg>
           </span>
