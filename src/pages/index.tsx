8847c4c460269a9caacfb078b1469a9547ef7a18
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-02 19:46:14 +0900

    fix blur

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ export const query = graphql`
           tags
           visual {
             childImageSharp {
-              gatsbyImageData
+              gatsbyImageData(placeholder: BLURRED)
             }
           }
         }
