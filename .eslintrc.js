fdf2e25325cbb8aa9f511b95a690172cf7acc7cb
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-03 23:04:45 +0900

    Introduce formatter (#249)
    * Format
    
    * ignore md
    
    * Compatible eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,39 +2,41 @@
 const DEFAULT_ORDER = ["builtin", "external", "parent", "sibling", "index"];
 
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: DEFAULT_ORDER,
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
     ],
-    settings: {
-        "import/resolver": {
-            typescript: true,
-            node: true
-        },
-
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module",
-        project: "./tsconfig.json",
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-
-    ],
-    "rules": {
-        "import/order": ["error", {"groups": DEFAULT_ORDER, "alphabetize": {"order": "asc", "caseInsensitive": true}}]
-    }
+  },
 };
