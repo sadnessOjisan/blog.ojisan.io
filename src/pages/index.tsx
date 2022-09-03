d628bc5026d3095db1d4cea66273157fe2cf426f
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-03 23:55:20 +0900

    fix format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import * as React from "react";
 const IndexPage = ({ data }: PageProps) => {
   return (
     <main>
-      NODE_ENV: {process.env["NODE_ENV"]} / 
-      GATSBY_ACTIVE_ENV: {process.env["GATSBY_ACTIVE_ENV"]}
+      NODE_ENV: {process.env["NODE_ENV"]} / GATSBY_ACTIVE_ENV:{" "}
+      {process.env["GATSBY_ACTIVE_ENV"]}
       {/* eslint-disable-next-line */}
       {/* @ts-ignore */}
       {data.allMarkdownRemark.nodes.map((node) => (
