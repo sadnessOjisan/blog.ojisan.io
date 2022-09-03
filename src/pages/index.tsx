245628b2f767af3b431c62ae5661a9ae31cb8263
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-03 23:49:38 +0900

    add logger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import * as React from "react";
 const IndexPage = ({ data }: PageProps) => {
   return (
     <main>
-      {process.env["NODE_ENV"]}
+      NODE_ENV: {process.env["NODE_ENV"]} / 
+      GATSBY_ACTIVE_ENV: {process.env["GATSBY_ACTIVE_ENV"]}
       {/* eslint-disable-next-line */}
       {/* @ts-ignore */}
       {data.allMarkdownRemark.nodes.map((node) => (
