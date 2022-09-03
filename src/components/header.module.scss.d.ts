412ca015dd32af6a4750705b23b7aaa6b8dea04b
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-03 16:57:02 +0900

    Cleanup

--- src/components/header.module.scss.d.ts
+++
@@ -1,4 +0,0 @@
-declare const styles: {
-  readonly header: string;
-};
-export = styles;
--- src/components/layout.module.scss.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly wrapper: string;
-  readonly footer: string;
-};
-export = styles;
