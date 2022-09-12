f3d347da66a0b93d643d8ab3d9a51809645a8928
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-13 03:10:36 +0900

    Show detail content

---
+++ src/pages/{MarkdownRemark.frontmatter__path}.tsx
@@ -0,0 +1,43 @@
+import { graphql, PageProps } from "gatsby";
+import { ComponentType } from "react";
+
+const Template: ComponentType<PageProps<Queries.BlogPostQuery>> = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>{props.data.markdownRemark?.frontmatter?.title}</p>
+      <div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: props.data.markdownRemark?.html || "",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Template;
+
+/**
+ * file system routing を使った場合、query が id に入ってくる。
+ * > Collection route components are passed two dynamic variables.
+ * > The id of each page’s node and the URL path as params.
+ * > The params is passed to the component as props.params and the id as props.pageContext.id.
+ * @see https://www.gatsbyjs.com/docs/reference/routing/file-system-route-api/#collection-route-components
+ */
+export const pageQuery = graphql`
+  query BlogPost($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        path
+        isProtect
+        created
+        tags
+      }
+      excerpt(pruneLength: 140)
+    }
+  }
+`;
