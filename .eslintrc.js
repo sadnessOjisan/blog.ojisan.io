fd88b8a878abbe7c77699e7b7f09cf62a18cff69
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-13 01:09:07 +0900

    Ignore react error

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
     "import/order": [
       "error",
       {
