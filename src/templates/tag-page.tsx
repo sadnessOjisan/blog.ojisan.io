c6ed48ec14f5699fe776b09fd9a853399153e9bb
sadnessOjisan <sadness.ojisan@gmail.com>
2022-10-04 23:51:20 +0900

    sort

--- src/templates/tag-page.tsx
+++ src/templates/tag-page.tsx
@@ -1,12 +1,7 @@
 import { graphql, PageProps } from "gatsby";
 import { CardList } from "../components/tags/card-list";
 
-const ListByTag = ({
-  data,
-  pageContext,
-}: PageProps<Queries.ArticlesByTagQuery>) => {
-  console.log(data);
-  console.log(pageContext);
+const ListByTag = ({ data }: PageProps<Queries.ArticlesByTagQuery>) => {
   return (
     <div>
       <CardList nodes={data.allMarkdownRemark.nodes} />
@@ -18,7 +13,10 @@ export default ListByTag;
 
 export const postsPaginationQuery = graphql`
   query ArticlesByTag($tag: String!) {
-    allMarkdownRemark(filter: { frontmatter: { tags: { in: [$tag] } } }) {
+    allMarkdownRemark(
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+      sort: { fields: [frontmatter___created], order: DESC }
+    ) {
       nodes {
         id
         frontmatter {
