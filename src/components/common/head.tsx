7fb6ba4ad3fb3506851a3996f01d26761decfb4e
sadnessOjisan <sadness.ojisan@gmail.com>
2022-10-05 03:13:55 +0900

    Add jsonld builder

--- src/components/common/head.tsx
+++ src/components/common/head.tsx
@@ -1,11 +1,17 @@
 import { graphql, useStaticQuery } from "gatsby";
-import { ComponentType, ReactNode } from "react";
+import { ComponentType } from "react";
 
 interface Props {
   title?: string;
+  imagePath?: string;
+  description?: string;
 }
 
-export const HeadFactory: ComponentType<Props> = ({ title, children }) => {
+/**
+ * head component として使われること想定
+ * @see: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const HeadFactory: ComponentType<Props> = ({ title, description }) => {
   // 他の場所でも呼び出すなら custom hooks として切り出すべき
   const siteMetaDataQueryResult: Queries.SiteMetaDataQuery =
     useStaticQuery(graphql`
@@ -21,17 +27,22 @@ export const HeadFactory: ComponentType<Props> = ({ title, children }) => {
         }
       }
     `);
-  console.log(siteMetaDataQueryResult);
-  if (!siteMetaDataQueryResult.site?.siteMetadata?.title) {
+  const baseData = siteMetaDataQueryResult.site?.siteMetadata;
+  if (!baseData?.title || !baseData?.description || !baseData?.siteUrl) {
     throw new Error("data source is invalid");
   }
   return (
     <>
-      <title>
-        {title
-          ? `${title} | ${siteMetaDataQueryResult.site.siteMetadata.title}`
-          : siteMetaDataQueryResult.site.siteMetadata.title}
-      </title>
+      <title>{title ? `${title} | ${baseData.title}` : baseData.title}</title>
+      <meta
+        name="description"
+        content={description ? description : baseData.description}
+      />
+      <meta name="twitter:url" content={baseData.siteUrl} />
+      <script type="application/ld+json">
+        {`{
+        }`}
+      </script>
     </>
   );
 };
