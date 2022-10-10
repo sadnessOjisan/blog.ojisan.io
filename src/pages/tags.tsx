4e62b14071806f3c0b801d0be23cdef7a8b95552
sadnessOjisan <sadness.ojisan@gmail.com>
2022-10-10 21:44:07 +0900

    Apply tags page

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,18 +1,21 @@
 import { graphql, Link, PageProps } from "gatsby";
 import { HeadFactory } from "../components/common/head";
 import { Layout } from "../components/common/layout";
+import * as styles from "./tags.module.css";
 
 const RootBlogList = ({ data }: PageProps<Queries.AllTagsQuery>) => {
   return (
     <Layout>
       <h1>タグ一覧</h1>
-      {data.tags.group.map((t) => (
-        <div key={t.tag}>
-          <Link to={`/tags/${t.tag}`}>
-            {t.tag}({t.totalCount})
-          </Link>
-        </div>
-      ))}
+      <ul className={styles.list}>
+        {data.tags.group.map((t) => (
+          <li key={t.tag} className={styles.listItem}>
+            <Link to={`/tags/${t.tag}`}>
+              {t.tag}({t.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
